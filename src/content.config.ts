c34cae294181a0471d8801e05a1ed4e1e39e087d
Luis Fonseca <luis.fonsecachinchilla@ucr.ac.cr>
2025-07-29 21:38:34 -0600

    Add md support

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,9 @@
-// 1. Import utilities from `astro:content`
 import { defineCollection, z } from 'astro:content';
 
-// 2. Import loader(s)
 import { glob } from 'astro/loaders';
 
-// 3. Define your collection(s)
 const blog = defineCollection({
-  loader: glob({ pattern: "**/*.mdx", base: "./src/data/blog" }),
+  loader: glob({ pattern: "**/*.{mdx,md}", base: "./src/data/blog" }),
   schema: z.object({
     title: z.string(),
     description: z.string(),
@@ -18,7 +15,4 @@ const blog = defineCollection({
   })
 });
 
-
-
-// 4. Export a single `collections` object to register your collection(s)
 export const collections = { blog };
